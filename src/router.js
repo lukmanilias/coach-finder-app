32d59b4fe3ea3d16b3e49b02973a0241907b849f
Lukman Hakim <lhakim885@gmail.com>
2024-07-31 00:24:54 +0800

    feat: registering routes and page components

---
+++ src/router.js
@@ -0,0 +1,46 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestList from './pages/requests/RequestList.vue';
+import NotFound from './pages/NotFound.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      redirect: '/coaches',
+    },
+    {
+      path: '/coaches',
+      component: CoachesList,
+    },
+    {
+      path: '/coaches/:id',
+      component: CoachDetail,
+      children: [
+        {
+          path: '/contact',
+          component: ContactCoach,
+        },
+      ],
+    },
+    {
+      path: '/register',
+      component: CoachRegistration,
+    },
+    {
+      path: '/requests',
+      component: RequestList,
+    },
+    {
+      path: '/:notFound(.*)',
+      component: NotFound,
+    },
+  ],
+});
+
+export default router;
