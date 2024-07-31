67bbeb7ef4c37d5b615ec74f646e2f2fb80394a9
Lukman Hakim <lhakim885@gmail.com>
2024-07-31 14:23:32 +0800

    feat: base components

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,26 +1,53 @@
 <template>
   <section>FILTER</section>
   <section>
-    <div class="controls">
-      <button>REFRESH</button>
-      <router-link to="/register">Register a Coach</router-link>
-    </div>
-    <ul v-if="hasCoaches">
-      LIST OF COACHES
-      <li v-for="coach in coachesList" :key="coach.id">
-        {{ coach.firstName }}
-      </li>
-    </ul>
-    <h3 v-else>No coaches found.</h3>
+    <base-card>
+      <div class="controls">
+        <base-button mode="outline">REFRESH</base-button>
+        <base-button link to="/register">Register as Coach</base-button>
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item
+          v-for="coach in coachesList"
+          :key="coach.id"
+          :id="coach.id"
+          :firstName="coach.firstName"
+          :lastName="coach.lastName"
+          :rate="coach.hourlyRate"
+          :areas="coach.areas"
+        ></coach-item>
+      </ul>
+      <h3 v-else>No coaches found.</h3>
+    </base-card>
   </section>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
 
+import CoachItem from '../../components/coaches/CoachItem.vue';
+import BaseButton from '../../components/ui/BaseButton.vue';
+
 export default {
+  components: {
+    CoachItem,
+    BaseButton,
+  },
   computed: {
     ...mapGetters('coaches', ['hasCoaches', 'coachesList']),
   },
 };
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
